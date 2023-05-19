725d35387e6327274a9201c70f6970658affbe6b
Alex Yashin <alex-yashin@ya.ru>
2023-05-19 19:03:10 +0400

    initial commit

---
+++ src/Dashboard.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace PinaDashboard;
+
+use Pina\Access;
+use Pina\App;
+use Pina\Container\NotFoundException;
+use Pina\Http\Location;
+use Pina\Url;
+
+class Dashboard
+{
+    protected $endpoints = [];
+
+    public function __construct()
+    {
+        Access::permit('admin', 'root');
+    }
+
+    public function register(string $pattern, $class, $context = [])
+    {
+        App::router()->register($this->getBasePattern() . '/' . $pattern, $class, $context);
+        $this->endpoints[$class] = $pattern;
+    }
+
+    public function getBasePattern()
+    {
+        return 'admin/:lang';
+    }
+
+    public function getBaseLocation(): Location
+    {
+        return new Location(Url::resource($this->getBasePattern(), ['lang' => 'en']));
+    }
+
+    public function getEndpointLocation($class): Location
+    {
+        if (!isset($this->endpoints[$class])) {
+            throw new NotFoundException();
+        }
+
+        $pattern = $this->endpoints[$class];
+        return $this->getBaseLocation()->location('@/' . $pattern);
+    }
+
+}
